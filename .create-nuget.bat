8b6f060c1c984e6cd49f07fafd1309913bf0d6b7
Martin Finkel <martin@videolabs.io>
2019-04-30 18:11:15 +0700

    nuget hack

--- .create-nuget.bat
+++ .create-nuget.bat
@@ -157,6 +157,7 @@ if "%DEBUG_VERSION%"=="" set DEBUG_VERSION=0
 set /a DEBUG_VERSION=%DEBUG_VERSION%+1
 pushd .nuspec
 %NUGET_EXE% pack Xamarin.Forms.nuspec -properties configuration=debug;platform=anycpu -Version 9.9.%DEBUG_VERSION%
+%NUGET_EXE% pack Xamarin.Forms.Platform.GTK.nuspec -properties configuration=debug;platform=anycpu -Version 9.9.%DEBUG_VERSION%
 if "%CREATE_MAP_NUGET%" NEQ "" (
 REM Requires building x86, x64, AMD
 	%NUGET_EXE% pack Xamarin.Forms.Maps.nuspec -properties configuration=debug;platform=anycpu -Version 9.9.%DEBUG_VERSION%
