18b24417034932dc52d6c60b0c4d09b7b4841a7c
Martin Finkel <martin@videolabs.io>
2019-05-05 09:55:09 +0200

    add gtk test project to XF repo

---
+++ GTK.Bug/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms.Sandbox;
+
+namespace GTK.Bug
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			global::Gtk.Application.Init();
+			Xamarin.Forms.Forms.Init();
+
+			var app = new App();
+			var window = new FormsWindow();
+			window.LoadApplication(app);
+			window.SetApplicationTitle("GTK# Forms sample");
+			window.Show();
+
+			global::Gtk.Application.Run();
+		}
+	}
+}
